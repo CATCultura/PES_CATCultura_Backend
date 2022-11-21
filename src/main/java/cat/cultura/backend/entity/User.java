61db9e0da284106cf011819e45d0a3ff09ac98e7
agusti <agustigm@gmail.com>
2022-11-21 17:38:08 +0100

    Problems with data truncation

--- src/main/java/cat/cultura/backend/entity/User.java
+++ src/main/java/cat/cultura/backend/entity/User.java
@@ -25,6 +25,7 @@ public class User {
     @Column(name="username", unique = true)
     private String username;
 
+    @Lob
     @Column(name="user_hash", unique = true)
     private String userHash;
 
