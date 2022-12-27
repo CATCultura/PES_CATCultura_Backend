307f2df5df1f297854d010c01f476760c8bcffd0
agusti <agustigm@gmail.com>
2022-12-27 19:08:53 +0100

    Fixed bug regarding parser

--- src/test/java/cat/cultura/backend/remoterequests/MockRequest.java
+++ src/test/java/cat/cultura/backend/remoterequests/MockRequest.java
@@ -6,8 +6,8 @@ public class MockRequest implements RequestStrategy{
     @Override
     public String performRequest(String requestUrl, String query) throws IOException {
         return "{" +
-                "\"1234\" : \"4.9\"," +
-                "\"5678\" : \"NaN\"" +
+                "\"1234\" : 4.9," +
+                "\"5678\" : NaN" +
                 "}";
     }
 }
--- src/test/java/cat/cultura/backend/utils/ScoreTest.java
+++ src/test/java/cat/cultura/backend/utils/ScoreTest.java
@@ -8,13 +8,14 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.Double.NaN;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScoreTest {
 
     @Test
     void parseScoreOk() {
-        Map.Entry<String,String> receivedScore = new MapEntry("123","7.9");
+        Map.Entry<String,Double> receivedScore = new MapEntry("123",7.9);
         Score expected = new Score();
         expected.setEventId(123L);
         expected.setSimilarityScore(7.9);
@@ -25,7 +26,7 @@ class ScoreTest {
 
     @Test
     void parseScoreNotOk() {
-        Map.Entry<String,String> receivedScore = new MapEntry("123","NaN");
+        Map.Entry<String,Double> receivedScore = new MapEntry("123",NaN);
         Score expected = new Score();
         expected.setEventId(123L);
         expected.setSimilarityScore(0.0);
