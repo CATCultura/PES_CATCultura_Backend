886b60d84bd85aa21915bc4a64b3192d775332a0
agusti <agustigm@gmail.com>
2022-12-27 10:37:15 +0100

    Added some tests.

--- src/test/java/cat/cultura/backend/service/EventServiceTest.java
+++ src/test/java/cat/cultura/backend/service/EventServiceTest.java
@@ -3,16 +3,22 @@ package cat.cultura.backend.service;
 
 import cat.cultura.backend.entity.Event;
 import cat.cultura.backend.exceptions.EventAlreadyCreatedException;
+import cat.cultura.backend.remoterequests.SimilarityServiceImpl;
 import cat.cultura.backend.repository.EventJpaRepository;
 import cat.cultura.backend.service.EventService;
+import cat.cultura.backend.utils.Score;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.BDDMockito.given;
 
@@ -24,6 +30,10 @@ class EventServiceTest {
 
     @MockBean
     EventJpaRepository eventJpaRepository;
+
+    @MockBean
+    SimilarityServiceImpl similarityService;
+
 
     @Test
     void saveEventTestAllOk() {
@@ -215,6 +225,55 @@ class EventServiceTest {
 
 
         Assertions.assertThrows(EventAlreadyCreatedException.class, () -> eventService.updateEvent(ev2));
+    }
+
+
+    @Test
+    void processSimilarityResultOk() throws IOException {
+        List<Score> remoteRequestResult = new ArrayList<>();
+        remoteRequestResult.add(new Score(123L,0.8));
+        remoteRequestResult.add(new Score(456L,0.2));
+
+        Event ev1 = new Event();
+        ev1.setId(123L);
+
+        Event ev2 = new Event();
+        ev2.setId(456L);
+
+        given(similarityService.getMostSimilar("whatever")).willReturn(remoteRequestResult);
+        given(eventJpaRepository.findById(123L)).willReturn(Optional.of(ev1));
+        given(eventJpaRepository.findById(456L)).willReturn(Optional.of(ev2));
+
+        List<Event> aux = new ArrayList<>();
+        aux.add(ev1);
+
+
+        Page<Event> result = eventService.getBySemanticSimilarity("whatever");
+
+        Assertions.assertEquals(new PageImpl<>(aux), result);
+    }
+
+    @Test
+    void processSimilarityResultVoid() throws IOException {
+        List<Score> remoteRequestResult = new ArrayList<>();
+        remoteRequestResult.add(new Score(123L,0.3));
+        remoteRequestResult.add(new Score(456L,0.2));
+
+        Event ev1 = new Event();
+        ev1.setId(123L);
+
+        Event ev2 = new Event();
+        ev2.setId(456L);
+
+        given(similarityService.getMostSimilar("whatever")).willReturn(remoteRequestResult);
+        given(eventJpaRepository.findById(123L)).willReturn(Optional.of(ev1));
+        given(eventJpaRepository.findById(456L)).willReturn(Optional.of(ev2));
+
+        List<Event> aux = new ArrayList<>();
+
+        Page<Event> result = eventService.getBySemanticSimilarity("whatever");
+
+        Assertions.assertEquals(new PageImpl<>(aux), result);
     }
 
 }
